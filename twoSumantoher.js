b336b53f0bc269d160e4b661b99bbfd945cf3836
Abu Sayed <abusayedtalukdersakib@gmail.com>
2023-01-11 22:03:48 +0200

    creating twoSum another way

---
+++ twoSumantoher.js
@@ -0,0 +1,19 @@
+//creating two sum 
+// from leetcode problem
+//easy 
+
+var twoSum = function(nums, target){
+    for(let i = 0; i < nums.length; i++){
+
+        let remaining = target - nums[i]
+        
+        for(let j = i + 1; j < nums.length; j++){
+            if(remaining === nums[j]) return [i, j]
+
+        }
+
+    }
+    return false
+
+}
+console.log(twoSum([2, 7, 11, 15, 3], 10))
